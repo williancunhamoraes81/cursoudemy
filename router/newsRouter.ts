51bc2e7e859b58508f252221a3ba731f76a56be9
Willian Cunha de Moraes <willian.cunha@totvs.com.br>
2021-03-15 15:38:56 -0300

    parcial

---
+++ router/newsRouter.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import NewsController from '../controller/newsController'
+
+const newsRouter = express.Router();
+
+newsRouter.route("/api/v1/news").get(NewsController.get);
+newsRouter.route("/api/v1/news/:id").get(NewsController.getById);
+newsRouter.route("/api/v1/news/search/:term").get(NewsController.search);
+newsRouter.route("/api/v1/news/export/tocsv").get(NewsController.exportToCsv); 
+newsRouter.route("/teste").get(NewsController.csvToLog); 
+newsRouter.route("/api/v1/news/").post(NewsController.create);
+newsRouter.route("/api/v1/news/:id").put(NewsController.update);
+newsRouter.route("/api/v1/news/:id").delete(NewsController.delete);
+
+export default newsRouter;
